157b8ef6994263f883c786c3510ee63468c988ac
MadPigeon <vsevolod007@gmail.com>
2013-11-16 00:03:22 +0600

    Добавлена команда Print

--- OOP_V0/Print.cpp
+++ OOP_V0/Print.cpp
@@ -1,24 +1,22 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <fstream>
 
 #include "Print.h"
+
 using namespace std;
-PrintCommand::PrintCommand(Application *appl) : name("print"),
-description("Выводит массив"),app(appl)
+
+PrintCommand::PrintCommand(Application *appl) : name("print"), description("Выводит массив"), app(appl) {}
+
+void PrintCommand::execute (vector <string> params)
 {
-	app = appl;
-}
-void PrintCommand:: execute (vector <string> params)
-{	
-	int i,n;
-	vector <int> mass;
-	mass = app -> get_arr();
-	n = mass.size();
+	vector <int> mass = app->get_arr();
+	int i, n = mass.size();
 	if(n == 0)
 	{
-		cout << "Количество итераций "<< app->get_iter() <<endl;
-		cout << "А массив настолько пуст, что %отличная шутка про пустой массив%\n";
+		cout << "Количество итераций " << app->get_iter() << endl;
+		cout << "А массив настолько пуст, что %шутка про пустой массив%\n";
 	}
 	else
 	{
@@ -28,6 +26,16 @@ void PrintCommand:: execute (vector <string> params)
 			cout << ' ' << mass[i] << ' ';
 		}
 		cout << endl << "————————————————————————————————————————————————————————————————————\n";
+		if (params[0] != "")
+		{
+			ofstream the_file(params[0]);
+			for (i = 0; i < n; i++)
+			{
+				the_file << mass[i] << " ";
+			}
+			the_file.close();
+			cout << "Также последовательность сохранена в файл: \"" << params[0] << "\"\n";
+		}
 	}
 }
 PrintCommand::~PrintCommand(void){}
