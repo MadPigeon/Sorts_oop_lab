ca644f046fbcb44a30e52e59563bb02267ecd35e
MadPigeon <vsevolod007@gmail.com>
2013-11-16 02:36:52 +0600

    Добавлено соблюдение дополнительного требования во второй лабораторной. Нуждается в тестировании, есть некие сомнения по поводу его полноценной работоспособности.

--- OOP_V0/Application.h
+++ OOP_V0/Application.h
@@ -9,10 +9,11 @@ using namespace std;
 // команд, поступающих с консоли и хранение информации,
 // специфичной для конкретного приложения
 class Application{
-	map<string,ICommand*> commandMap;
-	vector<ICommand *> commands;
+	map <string,ICommand*> commandMap;
+	vector <ICommand *> commands;
 	vector <int> arr;
 	long iter;
+	vector <string> commands_from_file;
 
 	void parse_str(string & input, vector<string> & list);
 public:
@@ -23,6 +24,8 @@ public:
 	virtual const vector<ICommand *> & get_command_list() const;		//  Возвращает список доступных команд
 	virtual void run(istream & in, ostream &out);	// Запускает цикл обработки команд
 	virtual void add_command(ICommand * cmd);	// Добавляет команду к списку
+
+	Application(int argc, char **argv);
 	
 	virtual vector <int> & get_arr();
 	virtual void add_to_arr(long val);
