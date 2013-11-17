f845315ab1c0b49c03b5c9090e0a6eb58c0b8721
MadPigeon <vsevolod007@gmail.com>
2013-11-17 19:55:33 +0600

    Косметические изменения.

--- OOP_V0/Exit.cpp
+++ OOP_V0/Exit.cpp
@@ -3,10 +3,10 @@
 
 void Exit::execute(vector <string> params)
 {
-    cout << "Âû õîòèòå ÇÀÊÐÛÒÜ ïðîãðàììó?! ÂÎÒ ÝÒÎ ÏÎÂÎÐÎÒ!";
+	cout << "Âû õîòèòå ÇÀÊÐÛÒÜ ïðîãðàììó?! ÂÎÒ ÝÒÎ ÏÎÂÎÐÎÒ!";
 	exit(0);
 }
-const string & Exit::get_name() const{	return name;}
-const string & Exit::get_help() const{	return description;}
+const string & Exit::get_name() const{ return name; }
+const string & Exit::get_help() const{ return description; }
 Exit::Exit(Application *appl): name("exit"), description("Âûçîä èç ïðîãðàììû"){}
 Exit::~Exit(){}
